def11eb59e0a70dec855bdeae102e6619c9d4100
Micke Carlsson <mickecamino@gmail.com>
2009-02-06 18:59:24 +0000

    Added GPL license text

--- page.announcement.php
+++ page.announcement.php
@@ -1,16 +1,21 @@
 <?php 
-/** Announcments module for FreePBX 2.2+
- * Copyright 2006 Greg MacLellan
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- */
+//This file is part of FreePBX.
+//
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+//
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
+//
+//    Announcments module for FreePBX 2.2+
+//    Copyright 2006 Greg MacLellan
 
 $action = isset($_POST['action']) ? $_POST['action'] :  '';
 if (isset($_POST['delete'])) $action = 'delete'; 
