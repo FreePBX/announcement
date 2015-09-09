120144b24b4205d4a54e235b89d087bf2378c43b
James Finstrom <jfinstrom@sangoma.com>
2015-09-09 09:26:01 -0700

    Adjust columns to be more meaningful. Show description and destination

--- views/grid.php
+++ views/grid.php
@@ -3,26 +3,27 @@
 //	Copyright 2015 Sangoma Technologies.
 //
 //
+$destlist = \FreePBX::Modules()->getDestinations();
 ?>
 <div id="toolbar-all">
 	<a href="config.php?display=announcement&amp;view=form" class="btn btn-primary"><i class="fa fa-plus"></i> <?php echo _('Add')?></a>
-	<button id="remove-all" class="btn btn-danger btn-remove" data-type="extensions" disabled data-section="all">
-		<i class="glyphicon glyphicon-remove"></i> <span><?php echo _('Delete')?></span>
-	</button>
 </div>
 <table data-toolbar="#toolbar-all" data-toggle="table"  data-maintain-selected="true" data-show-columns="true" data-show-toggle="true" data-toggle="table" data-pagination="true" data-search="true"  id="table-all">
 <thead>
 	<tr>
-		<th data-sortable="true"><?php echo _("ID")?></th>
 		<th data-sortable="true"><?php echo _("Description")?></th>
+		<th data-sortable="true"><?php echo _("Destination")?></th>
 		<th><?php echo _("Actions")?></th>
 	</tr>
 </thead>
 <tbody>
-	<?php foreach (announcement_list() as $row) {?>
+	<?php foreach (announcement_list() as $row) {
+		$postd = isset($destlist[$row['post_dest']]['description'])?$destlist[$row['post_dest']]['description']:$row['post_dest'];
+		$dest = $row['return_ivr'] == 1? _("Return to IVR"):$postd;
+		?>
 		<tr>
-			<td><?php echo $row['announcement_id']?></td>
 			<td><?php echo $row['description']?></td>
+			<td><?php echo $dest?></td>
 			<td>
 				<a href="?display=announcement&amp;view=form&amp;extdisplay=<?php echo $row['announcement_id']?>"><i class="fa fa-edit"></i></a>
 				<a href="?display=announcement&amp;action=delete&amp;extdisplay=<?php echo $row['announcement_id']?>" class="delAction"><i class="fa fa-trash"></i></a>
