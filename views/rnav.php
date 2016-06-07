23bf2fff96f9da6ff47e0104b6027ad6bb0b9722
James Finstrom <jfinstrom@sangoma.com>
2016-06-07 09:03:43 -0700

    FREEPBX-12477 Fix grids

--- views/rnav.php
+++ views/rnav.php
@@ -2,7 +2,7 @@
 	<a href="?display=announcement" class="btn btn-default"><i class="fa fa-list"></i> <?php echo _("List Announcements")?></a>
 	<a href="?display=announcement&amp;view=form" class="btn btn-default"><i class="fa fa-plus"></i> <?php echo _("Add Announcements")?></a>
 </div>
- <table id="destgrid-side" data-url="ajax.php?module=customappsreg&amp;command=getJSON&amp;jdata=destgrid" data-cache="false" data-toolbar="#toolbar-all" data-toggle="table" data-search="true" class="table">
+ <table id="announcegrid-side" data-url="ajax.php?module=announcement&amp;command=getJSON&amp;jdata=grid" data-cache="false" data-toolbar="#toolbar-all" data-toggle="table" data-search="true" class="table">
     <thead>
         <tr>
             <th data-field="description" data-sortable="true"><?php echo _("Destinations")?></th>
