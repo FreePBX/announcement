5228966d930ba58b8964ea3a666c4cfc5e5f78ad
Michael Newton <miken32@gmail.com>
2017-04-28 13:55:57 -0700

    add styling info

--- page.announcement.php
+++ page.announcement.php
@@ -15,7 +15,7 @@ switch($request["view"]){
 			$usage_list = framework_display_destination_usage(announcement_getdest($request['extdisplay']));
 			if(!empty($usage_list)){
 				$usagehtml = <<< HTML
-<div class="panel panel-default">
+<div class="panel panel-default fpbx-usageinfo">
 	<div class="panel-heading">
 		$usage_list[text]
 	</div>
